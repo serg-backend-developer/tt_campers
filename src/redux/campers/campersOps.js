e7cd9a1ef0f2a2987e90507bf5ce5ecc774f7665
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-14 20:24:43 +0200

    campers ops

---
+++ src/redux/campers/campersOps.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
+
+export const getAllCampers = createAsyncThunk(
+	"campers/getAll",
+	async (_, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.get("/campers");
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.message);
+		}
+	}
+);
+
+export const getCamperById = createAsyncThunk(
+	"campers/getCamperById",
+	async (camperId, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.get(`/campers/${camperId}`);
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.message);
+		}
+	}
+);
