2741c088c1065250ac57aea81fdb080b1e7b7ebc
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-14 20:31:28 +0200

    filters selectors

---
+++ src/redux/filters/filtersSelector.js
@@ -0,0 +1,3 @@
+export const selectEquipment = (state) => state.filters.equipment;
+export const selectLocation = (state) => state.filters.location;
+export const selectVehicleType = (state) => state.filters.vehicleType;
