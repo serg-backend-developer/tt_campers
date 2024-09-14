52fd4315f1e8d793de5b0b9934d523efc795829a
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-14 22:05:02 +0200

    icon component

---
+++ src/components/Icon.jsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx';
+
+import css from './Icon.module.css';
+
+const Icon = ({ name, className = '', active = false, ...props }) => (
+  <svg className={clsx(css.icon, css[className], { [css.active]: active })}
+    {...props}
+  >
+    <use xlinkHref={`/icons.svg#${name}`} />
+  </svg>
+);
+
+export default Icon;
