3d25145d8fbc353606d2603df317821bc40ba945
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-14 22:41:49 +0200

    general camper list

---
+++ src/components/GeneralCamperList.jsx
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
+import { changePage } from ',redux/campers/campersSlice';
+import { selectCampers, selectPage } from '.redux/campers/campersSelectors';
+import CamperList from './CamperList';
+import Button from './Button';
+import css from './GeneralCamperList.module.css';
+
+const GeneralCamperList = () => {
+  const dispatch = useDispatch();
+  const filteredItems = useSelector(selectCampers);
+  const page = useSelector(selectPage);
+  const itemsPerPage = 4;
+  const [visibleItems, setVisibleItems] = useState([]);
+
+  useEffect(() => {
+    const newVisibleItems = filteredItems.slice(0, itemsPerPage * page);
+    setVisibleItems(newVisibleItems);
+  }, [filteredItems, page]);
+
+  const handleClick = () => {
+    if (visibleItems.length < filteredItems.length) {
+      dispatch(changePage(page + 1));
+    }
+  };
+
+  const isLoadMoreVisible = visibleItems.length < filteredItems.length;
+
+  return (
+    <div className={css.container}>
+      {visibleItems.length > 0 ? (
+        <CamperList items={visibleItems} />
+      ) : (
+        <p className={css.abs}>No campers found.</p>
+      )}
+
+      {isLoadMoreVisible && (
+        <Button variant="outlined" onClick={handleClick} aria-label="Load more">
+          Load more
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default GeneralCamperList;
