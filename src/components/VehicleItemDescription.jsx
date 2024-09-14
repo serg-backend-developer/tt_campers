19d57345dd59f66a0bcf6b273432e660a4a2b7f7
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-14 22:10:36 +0200

    VehicleItemDescription

---
+++ src/components/VehicleItemDescription.jsx
@@ -0,0 +1,33 @@
+import { formatString, addSpaceToUnit } from './utils';
+import css from './VehicleItemDescription.module.css';
+
+const VehicleItemDescription = ({ form, length, width, height, tank, consumption }) => {
+  return (
+    <div className={css.content}>
+      <h4 className={css.title}>Vehicle Details</h4>
+      <hr className={css.trim} />
+      <ul className={css.list}>
+        <li className={css.item}>
+          Form <span>{formatString(form)}</span>
+        </li>
+        <li className={css.item}>
+          Length <span>{addSpaceToUnit(length)}</span>
+        </li>
+        <li className={css.item}>
+          Width <span>{addSpaceToUnit(width)}</span>
+        </li>
+        <li className={css.item}>
+          Height <span>{addSpaceToUnit(height)}</span>
+        </li>
+        <li className={css.item}>
+          Tank <span>{addSpaceToUnit(tank)}</span>
+        </li>
+        <li className={css.item}>
+          Consumption <span>{consumption}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default VehicleItemDescription;
