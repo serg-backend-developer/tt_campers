2d79589db2834a4bf6ee655f59f26f9bf9a1a443
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-14 22:22:33 +0200

    main header comp

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx';
+import { Link, NavLink } from 'react-router-dom';
+
+import Container from './Container';
+import logo from './assets/icon.svg';
+import css from './MainHeader.module.css';
+
+const MainHeader = () => {
+  return (
+    <header className={css.header}>
+      <Container>
+        <nav className={css.nav}>
+          <Link to="/" className={css.logo}>
+            <img src={logo} alt="Logo" />
+          </Link>
+
+          <ul className={css.list}>
+            <li className={css.item}>
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(css.link, isActive && css.active)
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className={css.item}>
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(css.link, isActive && css.active)
+                }
+                to="/catalog"
+              >
+                Catalog
+              </NavLink>
+            </li>
+            <li className={css.item}>
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(css.link, isActive && css.active)
+                }
+                to="/favorites"
+              >
+                Favorites
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
+};
+
+export default MainHeader;
