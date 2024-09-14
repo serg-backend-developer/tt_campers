c4980a4c7655dac83991be739442bf55232c518c
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-14 21:36:03 +0200

    camper component

---
+++ src/components/Camper.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+
+import Button from './Button';
+import CamperHeader from './CamperHeader';
+import EquipmentList from './EquipmentList';
+import css from './Camper.module.css';
+import { getFilteredVehicleEquipment } from './utils';
+
+const Camper = ({ camper }) => {
+  const { id, name, description, gallery = [] } = camper;
+
+  const equipment = getFilteredVehicleEquipment({ ...camper });
+
+  return (
+    <div className={css.card}>
+      <img src={gallery[0].thumb} alt={name} className={css.image} />
+
+      <div className={css.info}>
+        <CamperHeader camper={camper} />
+
+        <p className={css.description}>{description}</p>
+
+        <EquipmentList equipment={equipment} />
+
+        <Button aria-label="Show More">
+          <Link to={`/catalog/${id}/features`} className={css.link}>
+            Show More
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Camper;
