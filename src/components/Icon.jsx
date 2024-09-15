3fe2ada9702ae2879dac40c2f1ba4fe6c3570207
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-09-15 16:55:49 +0200

    icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -4,7 +4,7 @@ import css from './Icon.module.css';
 
 const Icon = ({ name, className = '', active = false, ...props }) => (
   <svg className={clsx(css.icon, css[className], { [css.active]: active })} {...props}>
-    <use xlinkHref={`/src/assets/all-icons.svg#${name}`} />
+    <use xlinkHref={`/icons.svg#${name}`} />
   </svg>
 );
 
